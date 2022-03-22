16dc00351bb7911bb762d00866a9c47dbc434990
Garv Choudhry <garvc07@Garvs-MacBook-Pro.local>
2022-03-22 15:09:41 -0400

    Finished Quad 1, Starting Quad 2

--- Sketch.java
+++ Sketch.java
@@ -16,7 +16,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255);
   }
 
   /**
@@ -24,13 +24,35 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // Dividing lines
+    stroke(0);
+    line(width / 2, 0, width / 2, height);
+    line(0, height / 2, width, height / 2);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // Quad 1
+    // Horizontal Lines
+    for(int intLineX = 0; intLineX <= width / 2; intLineX += width / 20){
+      fill(0);
+      line(intLineX, height / 2, intLineX, 0);
+    }
+    
+    // Vertical Lines 
+    for(int intLineY = 0; intLineY <= height / 2; intLineY += height / 20){
+      fill(0);
+      line(width / 2, intLineY, 0, intLineY);
+    }
+
+    /* Quad 2
+    int intCircleSize = width/20;
+    for(int intCircleX = width / 2 + intCircleSize; intCircleX > width / 2; intCircleX += width / 10){
+      for(int intCircleY = height / 20; intCircleY < height / 2; intCircleY += height / 10){
+        fill(0);
+        ellipse(intCircleX, intCircleY, intCircleSize, intCircleSize);
+      }
+    }
+    */
+
+
   }
   
-  // define other methods down here.
 }
