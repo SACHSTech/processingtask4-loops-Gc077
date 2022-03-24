5f5f5dfda96eea4d0247fadc4670c62b15c1c3db
Garv Choudhry <garvc07@Garvs-MacBook-Pro.local>
2022-03-24 14:29:45 -0400

    Completed Quad 2 and begun Quad 3

--- Sketch.java
+++ Sketch.java
@@ -26,29 +26,47 @@ public class Sketch extends PApplet {
 	  
     // Dividing lines
     stroke(0);
+    fill(0);
     line(width / 2, 0, width / 2, height);
     line(0, height / 2, width, height / 2);
 
     // Quad 1
     // Horizontal Lines
-    for(int intLineX = 0; intLineX <= width / 2; intLineX += width / 20){
+    for(int intLineX = 0; intLineX <= width / 2; intLineX += width / 20)
+    {
       fill(0);
       line(intLineX, height / 2, intLineX, 0);
     }
     
     // Vertical Lines 
-    for(int intLineY = 0; intLineY <= height / 2; intLineY += height / 20){
+    for(int intLineY = 0; intLineY <= height / 2; intLineY += height / 20)
+    {
       fill(0);
       line(width / 2, intLineY, 0, intLineY);
     }
 
-    /* Quad 2
-    int intCircleSize = width/20;
-    for(int intCircleX = width / 2 + intCircleSize; intCircleX > width / 2; intCircleX += width / 10){
-      for(int intCircleY = height / 20; intCircleY < height / 2; intCircleY += height / 10){
-        fill(0);
-        ellipse(intCircleX, intCircleY, intCircleSize, intCircleSize);
+    // Quad 2 
+    int intCircleSize = width / 20; // Circle Size variable 
+    for (int intCircleX = width / 2 + intCircleSize; intCircleX <= width - intCircleSize; intCircleX += width / 10)
+    {
+      for (int intCircleY = intCircleSize; intCircleY <= (height/2)-intCircleSize; intCircleY += height / 10 ) 
+      {
+        fill(200, 100, 200); // Purple
+          ellipse(intCircleX, intCircleY, intCircleSize, intCircleSize); // Draw Circles
       }
+    }
+
+    // Quad 3
+    for(int intGradientCol = 0; intGradientCol <= 255; intGradientCol ++)
+    {
+      stroke(intGradientCol);
+      line(intGradientCol, height / 2, intGradientCol, height / 2);
+    }
+    
+    /*for(int intGradientX = 0; intGradientX == width / 2; intGradientX ++)
+    {
+      stroke(intGradientCol);
+      line(intGradientX, height / 2, intGradientX, height / 2);
     }
     */
 
